c637629e8c1758f11b12bf2db51d9843f849159a
Alexander Lyon <arlyon@me.com>
2018-07-27 18:28:32 +0100

    update setup.py for new package

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 
 from setuptools import setup
 
@@ -10,12 +10,10 @@ setup(
     long_description=open('README.rst', 'r').read(),
     keywords=['design', 'pattern', 'circuit', 'breaker', 'integration'],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries',
     ],
     platforms=[
@@ -27,11 +25,8 @@ setup(
     url='http://github.com/danielfm/pybreaker',
     package_dir={'': 'src'},
     py_modules=['pybreaker'],
-    install_requires=[
-        'six',
-    ],
     include_package_data=True,
     zip_safe=False,
     test_suite='tests',
-    tests_require=['mock', 'fakeredis', 'redis', 'tornado'],
+    tests_require=['redis', 'fakeredis', 'aiounittest'],
 )
